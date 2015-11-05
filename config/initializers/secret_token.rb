83f2506e63b81613cbf67344ed0d3a7081de4f5f
saasbook <saasbook@saasbook.(none)>
2015-11-05 07:20:34 -0400

    Initialized my blog empty rails app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '0a6227b0d95a1149ac653641e943ba5f011fac43de2703d5f96821371be881bd99d982faa7c38cc984c895fcda32b41e4a2dea72ff3425da74f7b84c8e3ff192'
+Blog::Application.config.secret_token = '3f82a0bdda2110f24b05eed3c4f4a7975d74e42bdde086f918b88e96657597eb25e74e66087024fd63470cd4b52eb6dc3ca6bb317a118617341c1eeb1fa7bb12'
